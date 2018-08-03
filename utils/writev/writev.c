12afa1776803a70a14ef0b60dd38d78daa46cdfe
WilliamHammond <william.t.hammond@gmail.com>
2018-08-02 22:05:30 -0400

    working writev

--- utils/writev/writev.c
+++ utils/writev/writev.c
@@ -14,46 +14,56 @@ writev(int fd, const struct iovec *iov, int iovcnt) {
     char ch;
     size_t bufsize;
     ssize_t ret;
-    int *ptr;
+    char* buffer;
 
     bufsize = 0;
     for (int i = 0; i < iovcnt; i++) {
         bufsize += iov[i].iov_len; 
     }
-    ptr = malloc(bufsize);
+
+    buffer = (char *) malloc(bufsize);
+    char* bp = buffer;
     for (int i = 0; i < iovcnt; i++) {
-        memcpy(ptr, iov[i].iov_base, iov[i].iov_len);
+        bp = mempcpy(bp, iov[i].iov_base, iov[i].iov_len);
     }
-    ret = write(fd, ptr, bufsize);
+
+    ret = write(fd, buffer, bufsize);
     if (ret == -1) {
         printf("write: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
-    free(ptr);
+    free(buffer);
 
     return ret;
 }
 
 int 
 main(int argc, char *argv[]) {
-    int fd, flags, iovcnt;    
-    char x;
-    struct iovec iov[1];
+    int fd, flags, iovcnt, i;    
+    struct iovec iov[2];
     ssize_t ret; 
 
-    x = 'a';
-    iov[0].iov_base = &x;
-    iov[0].iov_len = sizeof(x);
+    char *buf[] = {
+                "The term buccaneer comes from the word boucan.\n",
+                "A boucan is a wooden frame used for cooking meat.\n",
+                "Buccaneer is the West Indies name for a pirate.\n" };
 
-    flags = O_CREAT | O_EXCL | O_RDWR;
+    for (i = 0; i < 3; i++) {
+        iov[i].iov_base = buf[i];
+        iov[i].iov_len = strlen(buf[i]) + 1;
+    }
+
+    flags = O_CREAT | O_TRUNC | O_WRONLY;
     fd = open(argv[1], flags);
     if (fd == -1) {
         printf("read: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    ret = writev(fd, iov, 1);
+    ret = writev(fd, iov, 3);
     printf("bytes written %zd", ret);
+
+    close(fd);
 
     exit(EXIT_SUCCESS);
 } 
